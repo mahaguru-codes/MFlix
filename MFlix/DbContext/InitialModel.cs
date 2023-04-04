75efd30ea8cb8a3259d1037bd05693527937d4ab
Saurabh Kumar <84146288+mahaguru-codes@users.noreply.github.com>
2023-04-04 17:24:15 +0530

    Integrated Entity Framework and migration

---
+++ MFlix/DbContext/InitialModel.cs
@@ -0,0 +1,27 @@
+﻿using MFlix.Models.Beans;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MFlix
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public string DbPath { get; }
+
+        public MovieContext()
+        {
+            var path = Environment.CurrentDirectory + @"\Database";
+            Directory.CreateDirectory(path);
+            DbPath = System.IO.Path.Join(path, "blogging.mdf");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
